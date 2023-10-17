f0a298e5b430ac3d2c141ba5e3e936848c78b25b
jimmyH <939867+jimmyH@users.noreply.github.com>
2023-10-17 09:32:10 +0100

    Add OpenTherm params to Device Details

--- src/app/device-details/device-details.component.ts
+++ src/app/device-details/device-details.component.ts
@@ -12,16 +12,29 @@ export class DeviceDetailsComponent implements OnInit {
   device: any;
   id: any;
   lastseen: Date = new Date(0);
+  dataTimerId: any;
 
   constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.refreshData();
+    this.dataTimerId = setInterval(() => { this.refreshData(); }, 5000);
+  }
+    
+  ngOnDestroy(): void {
+    clearInterval(this.dataTimerId);
+  }
+    
+  refreshData(){
     this.dataService.getDevice(this.route.snapshot.params.id).subscribe((data: any) =>{
       console.log(data);
       this.device = data;
       this.id = this.route.snapshot.params.id;
       this.lastseen = new Date(data.lastseen*1000);
     })
-  }
+  } 
+
+
+
 
 }
